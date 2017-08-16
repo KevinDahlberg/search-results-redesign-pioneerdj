4ae3cb42a1a50a6aa0c76236489e69b1cb14ec64
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-08-16 04:11:48 -0500

    added jquery to subheader

--- js/client.js
+++ js/client.js
@@ -1,1 +1,20 @@
 console.log('hello out there!');
+$(function() {
+  var divToCollapse = $('#js-sticky-wrap');
+  stickyDiv(divToCollapse);
+});
+
+
+function stickyDiv(el) {
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 161) {
+      el.stop().animate({
+        height: 50
+      }, 10, "linear");
+    } else {
+      el.stop().animate({
+        height: 100
+      }, 30, "linear");
+    }
+  });
+}
